d933ae3b729be0ab43b179301460d46786d01fc4
Xerlok <20791727+Xerlok@users.noreply.github.com>
2023-03-20 01:04:52 +0300

    Add basic operators js

---
+++ js/script.js
@@ -0,0 +1,37 @@
+function add(num1,num2) {
+    return num1 + num2;
+}
+
+function subtract(num1,num2) {
+    return num1 - num2;
+}
+
+function multiply(num1,num2) {
+    return num1 * num2;
+}
+
+function divide(num1,num2) {
+    return num1 / num2;
+}
+
+// not sure this one is correct, what is % in calculator?
+function modulo (num1,num2) {
+    return Math.pow(num1, num2);
+}
+
+// в степени
+function exponent (num1, num2) {
+    if (num2 == 2) {return num1 * num1}
+    if (num2 == 1) {return num1}
+    let result = num1;
+
+    for (i = 0; i < num2; i++) {
+        result = result * num1;
+    }
+
+    return result;
+}
+
+function square (num) {
+    return Math.sqrt(num);
+}
